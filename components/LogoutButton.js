94d30f5443ec756404d0786011abcd538e0186a6
rosenfence <rosenfence@gmail.com>
2023-04-21 11:54:49 +0900

    feat: gitHub OAuth 로그인/로그아웃 구현

---
+++ components/LogoutButton.js
@@ -0,0 +1,14 @@
+'use client';
+import { signOut } from 'next-auth/react';
+
+export default function LogoutButton() {
+  return (
+    <button
+      onClick={() => {
+        signOut();
+      }}
+    >
+      로그아웃
+    </button>
+  );
+}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,13 @@
 import Link from 'next/link';
+import LoginButton from './LoginButton';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import LogoutButton from './LogoutButton';
 
-export default function Navbar() {
+export default async function Navbar() {
+  let session = await getServerSession(authOptions);
+  console.log(session);
+
   return (
     <div className='navbar'>
       <Link prefetch={false} href='/' className='navbar-link'>
@@ -9,6 +16,8 @@ export default function Navbar() {
       <Link prefetch={false} href='/list' className='navbar-link'>
         📃
       </Link>
+      {session === null ? <></> : <span>{session.user.name}</span>}
+      {session === null ? <LoginButton /> : <LogoutButton />}
     </div>
   );
 }
